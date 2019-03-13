a701aa52c12d4dd3a7ecd769c91aa3a3d3fbb961
Osama Khaliq <osamakhaliq97@gmail.com>
2019-03-13 20:03:54 +0000

    build and compile Z3 library

--- install_deps.sh
+++ install_deps.sh
@@ -2,4 +2,12 @@
 rm -rf lib/*
 mkdir -p lib
 wget -O lib/fastjson-1.2.45.jar http://search.maven.org/remotecontent?filepath=com/alibaba/fastjson/1.2.45/fastjson-1.2.45.jar
-
+git clone https://github.com/Z3Prover/z3.git
+cd z3
+python scripts/mk_make.py --java
+cd build
+make examples
+ls
+cp ./com.microsoft.z3.jar ../../lib
+make
+make install
